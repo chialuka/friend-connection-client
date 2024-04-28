8d2ae5e27146f3e1908102bc0d285e193ff660e0
Chukwualuka Chiama <alukachiama@gmail.com>
2024-04-28 17:06:27 +0100

    add logout functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
-import { useAppSelector } from "../app/hooks";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { getGreeting } from "../utils/greeting";
+import { logout } from "../features/auth/authSlice";
 
 const Header = ({ scrollToLogin }: { scrollToLogin: () => void }) => {
 	const user = useAppSelector((state) => state.user.user);
+  const dispatch = useAppDispatch();
+
+  const logUserOut = () => {
+    dispatch(logout());
+  };
 
 	return (
 		<nav className="flex px-5 md:px-10 lg:px-20 text-2xl border-b-[1px] border-b-black shadow-md bg-[#1d85fc33]">
@@ -13,6 +19,7 @@ const Header = ({ scrollToLogin }: { scrollToLogin: () => void }) => {
 						<Link to="/posts">Posts</Link>
 						<Link to="/friends">Friends</Link>
 						<Link to="/members">Connect</Link>
+            <button className="text-xl" onClick={logUserOut}>Logout</button>
 					</div>
 					<p>
 						{getGreeting()} {user.username || user.email}
